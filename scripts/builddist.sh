c125c9e72080ea52deb0594dafa05678e42ce157
Robert Cunnings <nw8l@whitemesa.com>
2016-07-04 08:23:05 -0500

    MXE build
      * removed errant usr/ from build script

--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -5,7 +5,7 @@ make clean
 ./configure \
   $PKGCFG \
   $CROSSCFG \
-  --with-ptw32=$PREFIX/usr/i686-w64-mingw32.static \
+  --with-ptw32=$PREFIX/i686-w64-mingw32.static \
   --enable-static \
   PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
   FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config \
