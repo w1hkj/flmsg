2b3d1f529fefe419a5d3056b18e6e2081f92cb37
David Freese <iam_w1hkj@w1hkj.com>
2016-12-30 09:07:40 -0600

    buildmxe
      * removed
        - scripts/build4mingw.sh
        - scripts/build4mxe.sh
        - scripts/config4mingw.sh
      * added
        - scripts/buildmxe.sh

--- scripts/buildmxe.sh
+++ scripts/buildmxe.sh
@@ -1,6 +1,6 @@
-autoreconf
+#!/bin/sh
 
-make clean
+### Script to build a win32 installation
 
 ./configure \
   $PKGCFG \
@@ -10,10 +10,10 @@ make clean
   PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
   FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config \
 
+make clean
+
 make
 $PREFIX/bin/i686-w64-mingw32.static-strip src/flmsg.exe
 
 make nsisinst
 mv src/*setup*exe .
-
-make clean
