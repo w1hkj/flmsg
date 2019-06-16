c313e6acf7f4b640f8b68221b4cc22e82c38e5ad
Davide Gerhard <w1hkj@bellsouth.net>
2019-06-16 09:09:29 -0500

    timeops
      * fix for OS-X Xcode < 7.2

--- src/include/threads.h
+++ src/include/threads.h
@@ -26,91 +26,7 @@
 #include <pthread.h>
 #include <stdint.h>
 
-#include <semaphore.h>
-#if !HAVE_SEM_TIMEDWAIT
-#  include <time.h>
-int sem_timedwait(sem_t* sem, const struct timespec* abs_timeout);
-#endif
-
-int sem_timedwait_rel(sem_t* sem, double rel_timeout);
-int pthread_cond_timedwait_rel(pthread_cond_t* cond, pthread_mutex_t* mutex, double rel_timeout);
-
-enum {
-	INVALID_TID = -1,
-	TRX_TID, QRZ_TID, RIGCTL_TID, NORIGCTL_TID,
-#if USE_XMLRPC
-	XMLRPC_TID,
-#endif
-	ARQ_TID, ARQSOCKET_TID,
-	FLMAIN_TID,
-	NUM_THREADS, NUM_QRUNNER_THREADS = NUM_THREADS - 1
-};
-
-#ifdef __linux__
-void linux_log_tid(void);
-#  define LOG_THREAD_ID() linux_log_tid()
-#else
-#  define LOG_THREAD_ID()  /* nothing */
-#endif
-
-#if USE_TLS
-#       define THREAD_ID_TYPE __thread intptr_t
-#       define CREATE_THREAD_ID() thread_id_ = INVALID_TID
-#	define SET_THREAD_ID(x)   do { thread_id_ = (x); LOG_THREAD_ID(); } while (0)
-#	define GET_THREAD_ID()    thread_id_
-#else
-#       define THREAD_ID_TYPE pthread_key_t
-#	define CREATE_THREAD_ID() pthread_key_create(&thread_id_, NULL)
-#	define SET_THREAD_ID(x)   do { pthread_setspecific(thread_id_, (const void *)(x + 1)); LOG_THREAD_ID(); } while (0)
-#	define GET_THREAD_ID()    ((intptr_t)pthread_getspecific(thread_id_) - 1)
-#endif // USE_TLS
-extern THREAD_ID_TYPE thread_id_;
-
-
-#ifndef NDEBUG
-#  include "debug.h"
-bool thread_in_list(int id, const int* list);
-#  define ENSURE_THREAD(...)						\
-	do {								\
-		int id_ = GET_THREAD_ID();				\
-		int t_[] = { __VA_ARGS__, INVALID_TID };		\
-		if (!thread_in_list(id_, t_))				\
-			LOG_ERROR("bad thread context: %d", id_);	\
-	} while (0)
-#  define ENSURE_NOT_THREAD(...)					\
-	do {								\
-		int id_ = GET_THREAD_ID();				\
-		int t_[] = { __VA_ARGS__, INVALID_TID };		\
-		if (thread_in_list(id_, t_))				\
-			LOG_ERROR("bad thread context: %d", id_);	\
-	} while (0)
-#else
-#  define ENSURE_THREAD(...) ((void)0)
-#  define ENSURE_NOT_THREAD(...) ((void)0)
-#endif // ! NDEBUG
-
-
-// On POSIX systems we cancel threads by sending them SIGUSR2,
-// which will also interrupt blocking calls.  On woe32 we use
-// pthread_cancel and there is no good/sane way to interrupt.
-#ifndef __WOE32__
-#  define SET_THREAD_CANCEL()					\
-	do {							\
-		sigset_t usr2;					\
-		sigemptyset(&usr2);				\
-		sigaddset(&usr2, SIGUSR2);			\
-		pthread_sigmask(SIG_UNBLOCK, &usr2, NULL);	\
-	} while (0)
-#  define TEST_THREAD_CANCEL() /* nothing */
-#  define CANCEL_THREAD(t__) pthread_kill(t__, SIGUSR2)
-#else
-// threads have PTHREAD_CANCEL_ENABLE, PTHREAD_CANCEL_DEFERRED when created
-#  define SET_THREAD_CANCEL() /* nothing */
-#  define TEST_THREAD_CANCEL() pthread_testcancel()
-#  define CANCEL_THREAD(t__) pthread_cancel(t__);
-#endif
-
-#include "fl_lock.h"
+//#include "fl_lock.h"
 
 /// This ensures that a mutex is always unlocked when leaving a function or block.
 class guard_lock
@@ -120,20 +36,6 @@ public:
 	~guard_lock(void);
 private:
 	pthread_mutex_t* mutex;
-};
-
-/// This wraps together a mutex and a condition variable which are used
-/// together very often for queues etc...
-class syncobj
-{
-	pthread_mutex_t m_mutex ;
-	pthread_cond_t m_cond ;
-public:
-	syncobj();
-	~syncobj();
-	pthread_mutex_t * mtxp(void) { return & m_mutex; }
-	void signal();
-	bool wait( double seconds );
 };
 
 #endif // !THREADS_H_
--- src/utils/threads.cxx
+++ src/utils/threads.cxx
@@ -31,85 +31,7 @@
 #include <errno.h>
 #include "threads.h"
 
-THREAD_ID_TYPE thread_id_;
-
 #include "timeops.h"
-
-#ifndef __WIN32__
-#if !HAVE_SEM_TIMEDWAIT
-#  include <semaphore.h>
-#  include <time.h>
-#  include <errno.h>
-#  include <unistd.h>
-int sem_timedwait(sem_t* sem, const struct timespec* abs_timeout)
-{
-	int r;
-
-	for (;;) {
-		r = sem_trywait(sem);
-		if (r == 0 || (r == -1 && errno != EAGAIN))
-			return r;
-
-		if (abs_timeout->tv_nsec < 0 || abs_timeout->tv_nsec >= 1000000000L) {
-			errno = EINVAL;
-			return -1;
-		}
-
-		struct timespec now;
-		clock_gettime(CLOCK_REALTIME, &now);
-		if (now == *abs_timeout || now > *abs_timeout) {
-			errno = ETIMEDOUT;
-			return -1;
-		}
-
-		usleep(100);
-	}
-}
-#endif // !HAVE_SEM_TIMEDWAIT
-#endif // __WIN32__
-
-int sem_timedwait_rel(sem_t* sem, double rel_timeout)
-{
-        struct timespec t;
-        clock_gettime(CLOCK_REALTIME, &t);
-        t = t + rel_timeout;
-
-        return sem_timedwait(sem, &t);
-}
-
-int pthread_cond_timedwait_rel(pthread_cond_t* cond, pthread_mutex_t* mutex, double rel_timeout)
-{
-	struct timespec t;
-	clock_gettime(CLOCK_REALTIME, &t);
-	t = t + rel_timeout;
-
-	return pthread_cond_timedwait(cond, mutex, &t);
-}
-
-#ifndef NDEBUG
-bool thread_in_list(int id, const int* list)
-{
-	while (*list != INVALID_TID)
-		if (id == *list++)
-			return true;
-	return false;
-}
-#endif
-
-#ifdef __linux__
-#  ifndef _GNU_SOURCE
-#    define _GNU_SOURCE
-#  endif
-#  include <unistd.h>
-#  include <sys/syscall.h>
-#  include "debug.h"
-void linux_log_tid(void)
-{
-	LOG_DEBUG(PACKAGE_TARNAME " thread %" PRIdPTR " is LWP %ld", GET_THREAD_ID(), syscall(SYS_gettid));
-}
-#endif
-
-// Synchronization objects.
 
 guard_lock::guard_lock(pthread_mutex_t* m) : mutex(m)
 {
@@ -120,38 +42,4 @@ guard_lock::~guard_lock(void)
 {
 	pthread_mutex_unlock(mutex);
 }
-
-syncobj::syncobj()
-{
-	pthread_mutex_init( & m_mutex, NULL );
-	pthread_cond_init( & m_cond, NULL );
-}
-
-syncobj::~syncobj()
-{
-	pthread_mutex_destroy( & m_mutex );
-	pthread_cond_destroy( & m_cond );
-}
-
-void syncobj::signal()
-{
-	int rc = pthread_cond_signal( &m_cond );
-	if( rc )
-	{
-		throw std::runtime_error(strerror(rc));
-	}
-}
-
-bool syncobj::wait( double seconds )
-{
-	int rc = pthread_cond_timedwait_rel( &m_cond, &m_mutex, seconds );
-	switch( rc )
-	{
-	case 0 : return true ;
-	default : throw std::runtime_error(strerror(rc));
-	case ETIMEDOUT: return false ;
-	}
-}
-
-
 
