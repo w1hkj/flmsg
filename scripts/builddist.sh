0a8b5477bdb4b6148c32ea6eb64da0dbf067183f
dave-w1hkj <w1hkj@bellsouth.net>
2021-07-17 11:09:42 -0500

    Build scripts
      * update to build scripts

--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -1,24 +1,25 @@
 autoreconf
 
-make clean
+# build Windows installation file
 
 ./configure \
   $PKGCFG \
-  $CROSSCFG \
+  --host=i686-w64-mingw32.static \
   --with-ptw32=$PREFIX/i686-w64-mingw32.static \
   --enable-static \
   PTW32_LIBS="-lpthread -lpcreposix -lpcre" \
   FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config
 
+make clean
 make -j 4
 $PREFIX/bin/i686-w64-mingw32.static-strip src/flmsg.exe
-
 make nsisinst
 mv src/*setup*exe .
 
-make clean
-
 # build the distribution tarball
+
 ./configure
+
+make clean
 make distcheck
 make clean
--- scripts/buildmxe.sh
+++ scripts/buildmxe.sh
@@ -2,17 +2,17 @@
 
 ### Script to build a win32 installation
 
-make clean
-
 ./configure \
   $PKGCFG \
-  $CROSSCFG \
+  --host=i686-w64-mingw32.static \
   --with-ptw32=$PREFIX/i686-w64-mingw32.static \
   --enable-static \
   PTW32_LIBS="-lpthread -lpcreposix -lpcre" \
   FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config
 
+make clean
 make -j 4
+
 $PREFIX/bin/i686-w64-mingw32.static-strip src/flmsg.exe
 
 make nsisinst
